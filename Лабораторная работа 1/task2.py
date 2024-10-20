52fcd9f34a270e952c90383d449275c2aceb3b93
WildWestMouse <159918971+WildWestMouse@users.noreply.github.com>
2024-10-20 20:36:42 +0300

    Delete Лабораторная работа 1 directory

--- Лабораторная работа 1/task2.py
+++
@@ -1,8 +0,0 @@
-volume = 1.44 # В Мб
-num_pages = 100
-num_strings = 50
-num_symbols = 25
-vol_in_sym = 4 # В б
-
-count_books = int((volume * 1024 ** 2) // (num_pages * num_strings * num_symbols * vol_in_sym))
-print("Количество книг, помещающихся на дискету:", count_books)
