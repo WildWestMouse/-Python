54a900104e3abf2d55fc2e307a6918ef26fcc3b6
ShizaIncorp <159918971+WildWestMouse@users.noreply.github.com>
2024-10-23 00:34:21 +0300

    Исправил ошибки

--- Лабораторная работа 1/task2.py
+++ Лабораторная работа 1/task2.py
@@ -1,8 +1,8 @@
-volume = 1.44 # В Мб
-num_pages = 100
-num_strings = 50
-num_symbols = 25
-vol_in_sym = 4 # В б
+volume = 1.44  # В Мб
+number_pages = 100
+number_strings = 50
+number_symbols = 25
+symbol_volume = 4  # В б
 
-count_books = int((volume * 1024 ** 2) // (num_pages * num_strings * num_symbols * vol_in_sym))
+count_books = int((volume * 1024 ** 2) // (number_pages * number_strings * number_symbols * symbol_volume))
 print("Количество книг, помещающихся на дискету:", count_books)
