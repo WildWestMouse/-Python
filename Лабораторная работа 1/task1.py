95fb5eca7eceee98d3b4a2f8574c7e1a544e38bb
ShizaIncorp <159918971+WildWestMouse@users.noreply.github.com>
2024-10-23 21:33:53 +0300

    Update task1.py

--- Лабораторная работа 1/task1.py
+++ Лабораторная работа 1/task1.py
@@ -1,11 +1,11 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-varuable = 0
+variable = 0
 
-while numbers[varuable] is not None:
-  varuable += 1
+while numbers[variable] is not None:
+  variable += 1
 
-sum_ = sum(numbers[:varuable]) + sum(numbers[varuable + 1:])
+sum_ = sum(numbers[:variable]) + sum(numbers[variable + 1:])
 average_numbers = sum_ / len(numbers)
-numbers[varuable] = average_numbers
+numbers[variable] = average_numbers
 
 print("Измененный список:", numbers)
